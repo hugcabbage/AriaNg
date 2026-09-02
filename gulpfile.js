f84e13fce40f37b758439d3613c41987b08707da
hugcabbage <77980779+hugcabbage@users.noreply.github.com>
2026-09-02 16:42:52 +0000

    chore: upgrade build toolchain to Node 24 and refresh dependencies
    Raise the required Node version to >=24 and bring every devDependency up to date.
    
    - engines.node: >=14 -> >=24
    - bump browser-sync, del, eslint-plugin-angular, git-rev-sync, gulp-angular-templatecache, gulp-autoprefixer, gulp-eslint, gulp-replace, gulp-rev, gulp-size and nice-try to their latest releases
    - regenerate package-lock.json (lockfileVersion 3) on Node 24
    
    Rework gulpfile.js so it works with the refreshed toolchain: drop gulp-load-plugins (it can no longer resolve the new ESM-transpiled exports), require every plugin explicitly, read .default from ESM-transpiled modules (gulp-autoprefixer, gulp-rev, gulp-size and nice-try), and load del dynamically because del 8 is pure ESM.
    
    Verified on Node 24.20.0: both gulp clean build and gulp clean build-bundle complete successfully.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,37 @@
 const gulp = require('gulp');
-const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync');
-const del = require('del');
 const fs = require('fs');
 const git = require('git-rev-sync');
-const tryFn = require('nice-try');
+const tryFn = require('nice-try').default;
 const saveLicense = require('uglify-save-license');
 
-const $ = gulpLoadPlugins();
+const angularTemplatecache = require('gulp-angular-templatecache');
+const autoprefixer = require('gulp-autoprefixer').default;
+const cssnano = require('gulp-cssnano');
+const eslint = require('gulp-eslint');
+const gulpif = require('gulp-if');
+const htmlmin = require('gulp-htmlmin');
+const injectVersion = require('gulp-inject-version');
+const plumber = require('gulp-plumber');
+const replace = require('gulp-replace');
+const rev = require('gulp-rev').default;
+const revReplace = require('gulp-rev-replace');
+const size = require('gulp-size').default;
+const uglify = require('gulp-uglify');
+const useref = require('gulp-useref');
+
 const reload = browserSync.reload;
 
-gulp.task('clean', () => del(['.tmp', 'dist']));
+gulp.task('clean', async () => {
+    const { deleteSync } = await import('del');
+    deleteSync(['.tmp', 'dist']);
+});
 
 gulp.task('lint', () => gulp.src([
     'src/scripts/**/*.js'
 ]).pipe(reload({stream: true, once: true}))
-    .pipe($.eslint.format())
-    .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
+    .pipe(eslint.format())
+    .pipe(gulpif(!browserSync.active, eslint.failAfterError()))
     .pipe(gulp.dest('src/scripts')));
 
 gulp.task('prepare-fonts', () => gulp.src([
@@ -37,42 +52,42 @@ gulp.task('process-langs', gulp.series('prepare-langs', () => gulp.src([
 
 gulp.task('prepare-styles', () => gulp.src([
     'src/styles/**/*.css'
-]).pipe($.autoprefixer())
+]).pipe(autoprefixer())
     .pipe(gulp.dest('.tmp/styles'))
     .pipe(reload({stream: true})));
 
 gulp.task('prepare-scripts', () => gulp.src([
     'src/scripts/**/*.js'
-]).pipe($.plumber())
-    .pipe($.injectVersion({replace: '${ARIANG_VERSION}'}))
-    .pipe($.replace(/\${ARIANG_BUILD_COMMIT}/g, tryFn(git.short) || 'Local'))
+]).pipe(plumber())
+    .pipe(injectVersion({replace: '${ARIANG_VERSION}'}))
+    .pipe(replace(/\${ARIANG_BUILD_COMMIT}/g, tryFn(git.short) || 'Local'))
     .pipe(gulp.dest('.tmp/scripts'))
     .pipe(reload({stream: true})));
 
 gulp.task('prepare-views', () => gulp.src([
     'src/views/**/*.html'
-]).pipe($.htmlmin({collapseWhitespace: true}))
-    .pipe($.angularTemplatecache({module: 'ariaNg', filename: 'views/templates.js', root: 'views/'}))
+]).pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(angularTemplatecache({module: 'ariaNg', filename: 'views/templates.js', root: 'views/'}))
     .pipe(gulp.dest('.tmp/scripts')));
 
 gulp.task('prepare-html', gulp.series('prepare-styles', 'prepare-scripts', 'prepare-views', () => gulp.src([
     'src/*.html'
-]).pipe($.useref({searchPath: ['.tmp', 'src', '.']}))
-    .pipe($.if('js/*.js', $.replace(/\/\/# sourceMappingURL=.*/g, '')))
-    .pipe($.if('css/*.css', $.replace(/\/\*# sourceMappingURL=.* \*\/$/g, '')))
-    .pipe($.if(['js/moment-with-locales-*.min.js', 'js/plugins.min.js', 'js/aria-ng.min.js'], $.uglify({output: {comments: saveLicense}})))
-    .pipe($.if(['css/plugins.min.css', 'css/aria-ng.min.css'], $.cssnano({safe: true, autoprefixer: false})))
-    .pipe($.replace(/url\((\.\.\/fonts\/[a-zA-Z0-9\-]+\.woff2)(\?[a-zA-Z0-9\-_=.]+)?\)/g, (match, fileName) => {
+]).pipe(useref({searchPath: ['.tmp', 'src', '.']}))
+    .pipe(gulpif('js/*.js', replace(/\/\/# sourceMappingURL=.*/g, '')))
+    .pipe(gulpif('css/*.css', replace(/\/\*# sourceMappingURL=.* \*\/$/g, '')))
+    .pipe(gulpif(['js/moment-with-locales-*.min.js', 'js/plugins.min.js', 'js/aria-ng.min.js'], uglify({output: {comments: saveLicense}})))
+    .pipe(gulpif(['css/plugins.min.css', 'css/aria-ng.min.css'], cssnano({safe: true, autoprefixer: false})))
+    .pipe(replace(/url\((\.\.\/fonts\/[a-zA-Z0-9\-]+\.woff2)(\?[a-zA-Z0-9\-_=.]+)?\)/g, (match, fileName) => {
         return 'url(' + fileName + ')'; // remove version of woff2 file (woff2 file should be cached via application cache)
     }))
-    .pipe($.if(['js/plugins.min.js', 'js/aria-ng.min.js', 'css/plugins.min.css', 'css/aria-ng.min.css'], $.rev()))
-    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
-    .pipe($.revReplace())
+    .pipe(gulpif(['js/plugins.min.js', 'js/aria-ng.min.js', 'css/plugins.min.css', 'css/aria-ng.min.css'], rev()))
+    .pipe(gulpif('*.html', htmlmin({collapseWhitespace: true})))
+    .pipe(revReplace())
     .pipe(gulp.dest('.tmp'))));
 
 gulp.task('process-html', gulp.series('prepare-html', () => gulp.src([
     '.tmp/*.html'
-]).pipe($.replace(/<!-- AriaNg-Bundle:\w+ -->/, ''))
+]).pipe(replace(/<!-- AriaNg-Bundle:\w+ -->/, ''))
     .pipe(gulp.dest('dist'))));
 
 gulp.task('process-assets', gulp.series('process-html', () => gulp.src([
@@ -86,15 +101,15 @@ gulp.task('prepare-assets-bundle', () => gulp.src([
 ]).pipe(gulp.dest('.tmp')));
 
 gulp.task('process-assets-bundle', gulp.series('prepare-fonts', 'prepare-langs', 'prepare-html', 'prepare-assets-bundle', () => gulp.src('.tmp/index.html')
-    .pipe($.replace(/<link rel="stylesheet" href="(css\/[a-zA-Z0-9\-_.]+\.css)">/g, (match, fileName) => {
+    .pipe(replace(/<link rel="stylesheet" href="(css\/[a-zA-Z0-9\-_.]+\.css)">/g, (match, fileName) => {
         const content = fs.readFileSync('.tmp/' + fileName, 'utf8');
         return '<style type="text/css">' + content + '</style>';
     }))
-    .pipe($.replace(/<script src="(js\/[a-zA-Z0-9\-_.]+\.js)"><\/script>/g, (match, fileName) => {
+    .pipe(replace(/<script src="(js\/[a-zA-Z0-9\-_.]+\.js)"><\/script>/g, (match, fileName) => {
         const content = fs.readFileSync('.tmp/' + fileName, 'utf8');
         return '<script type="application/javascript">' + content + '</script>';
     }))
-    .pipe($.replace(/url\(\.\.\/(fonts\/[a-zA-Z0-9\-]+\.woff)(\?[a-zA-Z0-9\-_=.]+)?\)/g, (match, fileName) => {
+    .pipe(replace(/url\(\.\.\/(fonts\/[a-zA-Z0-9\-]+\.woff)(\?[a-zA-Z0-9\-_=.]+)?\)/g, (match, fileName) => {
         if (!fs.existsSync('.tmp/' + fileName)) {
             return match;
         }
@@ -103,7 +118,7 @@ gulp.task('process-assets-bundle', gulp.series('prepare-fonts', 'prepare-langs',
         const contentBase64 = contentBuffer.toString('base64');
         return 'url(data:application/x-font-woff;base64,' + contentBase64 + ')';
     }))
-    .pipe($.replace(/<link rel="icon" href="([a-zA-Z0-9\-_.]+\.png)">/g, (match, fileName) => {
+    .pipe(replace(/<link rel="icon" href="([a-zA-Z0-9\-_.]+\.png)">/g, (match, fileName) => {
         if (!fs.existsSync('.tmp/' + fileName)) {
             return match;
         }
@@ -112,7 +127,7 @@ gulp.task('process-assets-bundle', gulp.series('prepare-fonts', 'prepare-langs',
         const contentBase64 = contentBuffer.toString('base64');
         return '<link rel="icon" href="data:image/png;base64,' + contentBase64 + '">';
     }))
-    .pipe($.replace('<!-- AriaNg-Bundle:languages -->', () => {
+    .pipe(replace('<!-- AriaNg-Bundle:languages -->', () => {
         const langDir = '.tmp/langs/';
         let result = '';
         const fileNames = fs.readdirSync(langDir, 'utf8');
@@ -139,7 +154,7 @@ gulp.task('process-assets-bundle', gulp.series('prepare-fonts', 'prepare-langs',
 
         return result;
     }))
-    .pipe($.replace(/<[a-z]+( [a-z\-]+="[a-zA-Z0-9\- ]+")* [a-z\-]+="((favicon.ico)|(tileicon.png)|(touchicon.png))"\/?>/g, ''))
+    .pipe(replace(/<[a-z]+( [a-z\-]+="[a-zA-Z0-9\- ]+")* [a-z\-]+="((favicon.ico)|(tileicon.png)|(touchicon.png))"\/?>/g, ''))
     .pipe(gulp.dest('dist'))));
 
 gulp.task('process-full-extras', () => gulp.src([
@@ -156,7 +171,7 @@ gulp.task('process-tiny-extras', () => gulp.src([
 
 gulp.task('info', () => gulp.src([
     'dist/**/*'
-]).pipe($.size({title: 'build', gzip: true})));
+]).pipe(size({title: 'build', gzip: true})));
 
 gulp.task('build', gulp.series('lint', 'process-fonts', 'process-langs', 'process-assets', 'process-full-extras', 'info'));
 
